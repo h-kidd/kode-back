995bb7cf9a04b17579351e0688fbf9c5d001ec23
philine1 <phparkeu93@gmail.com>
2022-02-25 13:42:48 +0000

    creating classes for db

--- server.py
+++ server.py
@@ -11,14 +11,46 @@ CORS(app)
 socket = SocketIO(app, cors_allowed_origins="*")
 app.config.from_object(config("APP_SETTINGS"))
 
+# "///" is relative path from current file.
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///kode.db"
+
+# Creating db instance
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class Student(db.Model):
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.String(65), nullable=False)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    homework = db.Column(db.String(10), nullable=False, default="0")
+    completed = db.Column(db.String(10), nullable=False, default="0")
+
+    # how object is printed
+    def __repr__(self):
+        return f"Student('{self.firstname}','{self.lastname} ')"
+
+# Teacher and Student relationship: one to many.
+class Teacher(db.Model):
+    username = db.Column(db.String(20), primary_key=True)
+    #students attribute has relationship to Student model,
+    #backref is similar to adding another column to the Student model.
+    #Allows when there is a teacher, use student attribute to get the Student
+    #linked to teacher. Lazy arg is used to load necessary data in one go.
+    students = db.relationship("Student", backref="student", lazy=True)
+
+
+
+
+
+
+# Routes
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return {'message': 'Hello!'}
 
+# socket
 @socket.on('create')
 def on_join(room):
     socket.join_room(room)
