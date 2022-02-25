898d165df2da3f8999135ce917fbe21a9f2fcf33
Hayden Kidd <hkidd@live.co.uk>
2022-02-25 11:12:33 +0000

    added socketio setup

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return {'message': 'Hello!'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
