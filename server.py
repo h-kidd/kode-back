45fd24e21e0bace5fba79902188c4b6d07935161
NoamaanMohamed <noamaan.mohamed@hotmail.co.uk>
2022-02-25 15:15:39 +0000

    added homework and completed

--- server.py
+++ server.py
@@ -8,12 +8,13 @@ from decouple import config
 
 
 app = Flask(__name__)
+# "///" is relative path from current file.
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///kode.db"
 CORS(app)
 socket = SocketIO(app, cors_allowed_origins="*")
 # app.config.from_object(config("APP_SETTINGS"))
 
-# "///" is relative path from current file.
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///kode.db"
+
 
 # Creating db instance
 db = SQLAlchemy(app)
@@ -27,6 +28,9 @@ class Student(db.Model):
     firstname = db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey("teacher.id"), nullable=False )
+    homework_id = db.Column(db.Integer, db.ForeignKey("homework.id"), nullable=False )
+    homework_id = db.Column(db.Integer, db.ForeignKey("completed.id"), nullable=False )
+
     # homework = db.Column(db.String(10), nullable=False, default="0")
     # completed = db.Column(db.String(10), nullable=False, default="0")
 
@@ -49,6 +53,19 @@ class Teacher(db.Model):
     def __repr__(self):
         return f"Teacher('{self.firstname}','{self.lastname} ')"
 
+
+class Homework(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exercise = db.Column(db.String(20), nullable=False)
+    difficulty = db.Column(db.String(20), nullable=False)
+    students = db.relationship("Student", backref="student", lazy=True)
+
+class Completed(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exercise = db.Column(db.String(20), nullable=False)
+    difficulty = db.Column(db.String(20), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    students = db.relationship("Student", backref="student", lazy=True)
 
 
 
@@ -82,6 +99,7 @@ def on_send_score(data):
     name = data['name']
     score = data['score']
     socket.emit('user_score', {'name': name, 'score': score}, room=room)
+
 
 if __name__ == '__main__':
     socket.run(app)
