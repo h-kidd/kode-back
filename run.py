e667c24f9a27b08f28dbd6ccbb347de796c62248
Hayden Kidd <hkidd@live.co.uk>
2022-03-02 11:03:06 +0000

    socket fix

--- run.py
+++ run.py
@@ -1,28 +1,33 @@
 from server import socket, app
+from flask_socketio import join_room, emit
 
 # socket
 @socket.on('create')
 def on_join(room):
-    socket.join_room(room)
+    join_room(room)
 
 @socket.on('join')
 def on_join(data):
     room = data['room']
-    name = data['name']
-    socket.join_room(room)
-    socket.emit('user_joined', name, room=room)
+    firstname = data['firstname']
+    lastname = data['lastname']
+    join_room(room)
+    emit('user_joined', {'firstname': firstname, 'lastname': lastname}, room=room)
 
 @socket.on('start_game')
 def on_join(data):
     room = data['room']
-    socket.emit('init_game', "Game start!", room=room)
+    topic = data['topic']
+    difficulty = data['difficulty']
+    emit('init_game', {'topic': topic, 'difficulty': difficulty}, room=room)
 
 @socket.on('send_score')
 def on_send_score(data):
     room = data['room']
-    name = data['name']
+    firstname = data['firstname']
+    lastname = data['lastname']
     score = data['score']
-    socket.emit('user_score', {'name': name, 'score': score}, room=room)
+    emit('user_score', {'firstname': firstname, 'lastname': lastname, 'score': score}, room=room)
 
 
 if __name__ == '__main__':
